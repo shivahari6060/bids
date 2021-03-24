9160df7ab70e9ba3f48305d8a8ce64be7d6ca9dd
shivahari6060 <devkota.shivahari@gmail.com>
2021-03-24 15:14:48 +0545

    new

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,7 @@ from .views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', index, name= 'index' ),
-    path('login', Login, name= 'login' ),
+    path('', Login, name= 'login' ),
     path('register', Signup, name= 'register' ),
     path('logout', Logout, name= 'logout' ),
 ]
