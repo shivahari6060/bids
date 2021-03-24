bb7bb362b91f19fda031427e7d0902ba6e478f1d
shivahari6060 <devkota.shivahari@gmail.com>
2021-03-24 16:10:00 +0545

    a

--- bids/urls.py
+++ bids/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('ecom/', include('ecom.urls')),
     path('bidding/', include('bidding.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
